3eee2fadfd0fc80612fd901a67bb7ccaec4cd258
frantsuhvika@gmail.com <frantsuhvika@gmail.com>
2020-06-19 15:20:21 +0300

    added style.css for home page and connect bootstrap, added jumbotron

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 const express = require("express")
+const path = require("path")
 
 const port= process.env.PORT || 5000
 
 const app = express()
 
+const publicDir = path.join(__dirname, './public')
+app.use(express.static(publicDir))
+//console.log(__dirname)
+
+//connect .hbs file
+app.set('view engine', 'hbs')
+
+// add database
+
+
+
+//add pages
 app.get("/", (req, res)=>{
-    res.send("Home page")
+    //res.send("Home page")
+    res.render("index.hbs")
 })
 
 
 app.get("/register", (req, res)=>{
-    res.send("Register page")
+   // res.send("Register page")
+   res.render("register")
 })
 
 
